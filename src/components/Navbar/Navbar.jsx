0d23118b40044653bfb49a26fca950c59ca1e467
Samuel <sam.saravia2010@gmail.com>
2023-08-27 17:54:29 -0600

    feat: add the global colors to project

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,12 @@
+import * as S from './Navbar.styles';
+
+const Navbar = ()=>{
+
+    return(
+        <S.ContainerNavbars>
+            mi primer style
+        </S.ContainerNavbars>
+    )
+}
+
+export default Navbar;
---
+++ src/components/Navbar/Navbar.styles.jsx
@@ -0,0 +1,7 @@
+import  styled from "styled-components";
+import { colors } from "../../styles/colors";
+
+export const ContainerNavbars = styled.div`
+height:50px;
+background-color:${colors.BACKGROUND_GRADIENT_PINK};
+`;
