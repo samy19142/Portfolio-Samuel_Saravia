88bc11be07f0d374e45a1ba9cea2a52aea4b8e84
Samuel <sam.saravia2010@gmail.com>
2023-09-06 20:51:41 -0600

    feat: add the component navbar with Hamburguer Menu

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,12 +1,33 @@
-import * as S from './Navbar.styles';
+import * as S from "./Navbar.styles";
+import { useState } from "react";
+const Navbar = () => {
+  const [showMenuBurguer,setShowMenuBurguer]= useState(false);
 
-const Navbar = ()=>{
 
-    return(
-        <S.ContainerNavbars>
-            mi primer style
-        </S.ContainerNavbars>
-    )
-}
+  return (
+    <S.ContainerNavbars>
+      <S.ContainerLogo>
+        <S.ImageLogo src="/img/Logo.png" alt="Logo Samuel Saravia" />
+        <div>
+          <h1>Samuel Saravia</h1>
+          <h2>Fronted Developer</h2>
+        </div>
+      </S.ContainerLogo>
+
+      <S.ContainerBurguer onClick={()=>setShowMenuBurguer(!showMenuBurguer)}>
+        <S.BurguerMenu></S.BurguerMenu>
+        <S.BurguerMenu></S.BurguerMenu>
+        <S.BurguerMenu></S.BurguerMenu>        
+      </S.ContainerBurguer>
+
+      <S.Containeritems showMenuBurguer={showMenuBurguer}>
+        <p>About me</p>
+        <p>Projects</p>
+        <S.Button>Contact me</S.Button>
+      </S.Containeritems>
+
+    </S.ContainerNavbars>
+  );
+};
 
 export default Navbar;
--- src/components/Navbar/Navbar.styles.jsx
+++ src/components/Navbar/Navbar.styles.jsx
@@ -1,7 +1,106 @@
-import  styled from "styled-components";
+import styled from "styled-components";
 import { colors } from "../../styles/colors";
 
 export const ContainerNavbars = styled.div`
-height:50px;
-background-color:${colors.BACKGROUND_GRADIENT_PINK};
+  max-width: 100vw;
+  height: 100px;
+  box-shadow: ${colors.BOX_SHADOW};
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 0 20px;
+  background: ${colors.TEXT_WHITE};
+`;
+
+export const ContainerLogo = styled.div`
+  display: flex;
+  align-items: center;
+
+  h1,
+  h2 {
+    font-weight: 700;
+    color: ${colors.TEXT_BLACK};
+  }
+
+  h1 {
+    font-size: 2rem;
+    font-weight:800;
+  }
+
+  h2 {
+    background: linear-gradient(135deg, #cb5eee 0%, #4be1ec 100%);
+    background-clip: text;
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;
+  }
+`;
+
+export const ImageLogo = styled.img`
+  width: 90px;
+  padding-right: 20px;
+  border-radius: 50px;
+`;
+
+export const Containeritems = styled.div`
+   display:flex;
+   align-items:center;
+   p{
+    cursor: pointer;
+    padding:0 13px;
+    text-decoration:none;
+    color: ${colors.TEXT_BLACK};
+    font-weight:500;
+   }
+
+   @media (max-width:768px) {
+    display: ${(props)=>(props.showMenuBurguer ?'inline-grid':'none')}; 
+    position:absolute;
+    top:100px;
+    background: ${colors.TEXT_WHITE};
+    box-shadow: ${colors.BOX_SHADOW};
+    padding:10px 0 20px 0;
+    width:100%;
+    left:0;
+    text-align:center;
+
+    a{
+      padding:10px 20px;
+    }
+
+  }
+`;
+
+export const Button = styled.button`
+    color: ${colors.TEXT_WHITE};
+    box-shadow:${colors.BOX_SHADOW_BUTTON};
+    background: linear-gradient(135deg, #6699ff 0%, #ff3366 100%);
+    padding: 12px 20px;
+    border-radius:999px;
+    border:none;
+    margin:10px 0 ;
+    font-size: 16px;
+    font-weight:700;
+    cursor: pointer;
+  
+    
+
+    @media (max-width>768px) {
+        width:50%;
+        margin:5px 10px 0 10px;   
+    }
+`;
+
+export const ContainerBurguer = styled.div`
+  display:none;
+
+  @media (max-width:768px) {
+    display:block;
+  }
+`;
+
+export const BurguerMenu = styled.div`
+  width:35px;
+  height:2px;
+  background-color: ${colors.TEXT_BLACK};
+  margin:6px 0;
 `;
